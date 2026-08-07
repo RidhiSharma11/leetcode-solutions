1939167de3598f1fc0670a9dad40055529c7d25a
Ridhi Sharma <theridhiisharmaa@gmail.com>
2026-08-07 12:25:57 +0530

    Time: 10 ms (94.2%), Space: 46.7 MB (39.87%) - LeetHub

---
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        Arrays.sort(nums);
+        int i = 0;
+        int j = 1;
+        int count = 0;
+        while(j<nums.length){
+            if(i==j){
+                j++;
+                continue;
+            }
+            int diff = nums[j]-nums[i];
+            if(diff<k) j++;
+            else if(diff>k) i++;
+            else {
+                count++;
+                i++;
+                j++;
+               while (i < nums.length && nums[i] == nums[i - 1]) {
+                    i++;
+                }
+                while (j < nums.length && nums[j] == nums[j - 1]) {
+                    j++;
+                }
+            }
+        }
+        return count;
+    }
+}
